303b4f904a122263337e229d8b9d81af0e915b86
Max Dmitriev <max.dmitriev@6point6.co.uk>
2018-09-12 10:44:38 +0100

    page templates modified

--- website-content/themes/jsarc/page-two-columns-sidebar-right.php
+++ website-content/themes/jsarc/page-two-columns-sidebar-right.php
@@ -11,7 +11,8 @@
 
 get_header();
 ?>
-	<main>
+		<main>
+		<h2>Template Name: Two Columns Right Sidebar</h2>
 		<div class="row">
 			<div class="column large-4 small-12">Column 1</div>
 			<div class="column large-4 small-12">Column 2</div>
@@ -19,7 +20,19 @@ get_header();
 				<div class="sidebar-content">Sidebar</div>
 			</aside>
 		</div>
-		
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
