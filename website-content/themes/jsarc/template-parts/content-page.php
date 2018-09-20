4fbd74ed41a74dc37f411035e15f1743a6d331be
Pete Richardson <peter.richardson@6point6.co.uk>
2018-09-20 10:24:26 +0100

    remove unecessary tags from content-page base template

--- website-content/themes/jsarc/template-parts/content-page.php
+++ website-content/themes/jsarc/template-parts/content-page.php
@@ -6,47 +6,6 @@
  *
  * @package JSaRC
  */
-
 ?>
-
-<article <?php post_class(); ?>>
-	<header>
-		<?php the_title( '<h1>', '</h1>' ); ?>
-	</header><!-- .entry-header -->
-
-	<?php jsarc_post_thumbnail(); ?>
-
-	<div>
 		<?php
 		the_content();
-
-		wp_link_pages( array(
-			'before' => '<div>' . esc_html__( 'Pages:', 'jsarc' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer>
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span>%s</span>', 'jsarc' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					get_the_title()
-				),
-				'<span>',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
-</article><!-- #post-<?php the_ID(); ?> -->
