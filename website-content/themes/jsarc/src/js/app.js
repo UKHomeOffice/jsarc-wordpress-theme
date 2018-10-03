be97e9166984d73fbf1dfe5c99b7fc0764df7898
Pete Richardson <peter.richardson@6point6.co.uk>
2018-10-03 12:11:02 +0100

    update app.js for text input, archive previous homepage static prototype

--- website-content/themes/jsarc/src/js/app.js
+++ website-content/themes/jsarc/src/js/app.js
@@ -57,13 +57,13 @@ window.addEventListener('scroll', function() {
 	if(active) {
 		active.classList.remove('active');
 	}
-	
+
 });
 */
 
 
-// close dropdown & serach field if clicked outside the nav 
- 
+// close dropdown & serach field if clicked outside the nav
+
 document.addEventListener('click', function(event) {
   var isClickInside = primaryNav.contains(event.target);
 
@@ -78,7 +78,7 @@ document.addEventListener('click', function(event) {
 	*/
 	// close search field
 	primaryNav.classList.remove('searchActive');
-	
+
   }
 });
 
@@ -91,10 +91,8 @@ var videoOverlayTrigger = document.getElementById('videoOverlayTrigger');
 var videoOverlay = document.getElementById('videoOverlay');
 var videoOverlayClose = document.getElementById('videoOverlayClose');
 
-
-
 videoOverlayTrigger.addEventListener('click', function(e) {
-	
+
 	e.preventDefault();
 	videoOverlay.classList.add('active');
 });
@@ -102,9 +100,32 @@ videoOverlayTrigger.addEventListener('click', function(e) {
 videoOverlayClose.addEventListener('click', function() {
 
 	videoOverlay.classList.remove('active');
-	
+
 
 });
+
+
+
+
+
+// Register for open days email input
+// (animated floating label style text input)
+var registerTextInput = document.querySelector('.register-input__text-field');
+
+if (registerTextInput) {
+
+  registerTextInput.addEventListener('change', function() {
+
+    // When text input field is changed, if it contains content add 'active' class
+    // To ensure input label remains floated above the input if text has been added
+    if (registerTextInput && registerTextInput.value) {
+      registerTextInput.classList.add('active');
+    }
+    else {
+      registerTextInput.classList.remove('active');
+    }
+  });
+}
 
 
 }, false);
