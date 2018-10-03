ca87326f6781b034c30d4e71d1870796baf517b2
Pete Richardson <peter.richardson@6point6.co.uk>
2018-10-03 11:51:56 +0100

    add animated input and further mobile layout styling to homepage_v2 prototype

--- website-content/themes/jsarc/workshop/homepage_v2/app.bundle.js
+++ website-content/themes/jsarc/workshop/homepage_v2/app.bundle.js
@@ -85,3 +85,28 @@ document.addEventListener('DOMContentLoaded', function () {
     videoOverlay.classList.remove('active');
   });
 }, false);
+
+
+
+
+
+
+
+// Register for opendays email input - (Animated floating label input)
+var registerTextInput = document.querySelector('.register-input__text-field');
+
+if (registerTextInput) {
+
+  // When text input field is changed, if it contains content add 'active' class
+  // so the input label remains floated above the input
+  registerTextInput.addEventListener('change', function() {
+    if (registerTextInput && registerTextInput.value) {
+      registerTextInput.classList.add('active');
+    }
+    else {
+      registerTextInput.classList.remove('active');
+    }
+  });
+}
+
+
