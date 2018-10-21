0290422405a6a90d5aea64e25f2fc89a0d616b01
Andrew Tarry <andrew.tarry@6point6.co.uk>
2018-10-21 12:53:09 +0100

    moved theme

---
+++ website-content/wp-content/themes/jsarc/footer.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package JSaRC
+ */
+
+?>
+
+<footer class="section section-footer">
+  <div class="section-content">
+    <div class="row">
+      <div class="column large-4 small-5">
+        <ul>
+          <li class="menu-item"><a class="text-link" href="#">Contact JSaRC</a></li>
+          <li class="menu-item"><a class="text-link" href="#">Terms and Conditions</a></li>
+          <li class="menu-item"><a class="text-link" href="#">Privacy Policy</a></li>
+        </ul>
+      </div>
+      <div class="column large-4 small-5 small-offset-1">
+        <ul>
+          <li class="menu-item"><a class="text-link" href="#">Cookie Policy</a></li>
+          <li class="menu-item"><a class="text-link" href="#">Sitemap</a></li>
+        </ul>
+      </div>
+      <div class="column large-4 small-12">
+        <figure class="organisation-logo" alt="Home Office Logo"></figure>
+        <small class="copyright">© 2018 Joint Security and Resilience Centre</small>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ website-content/wp-content/themes/jsarc/src/gulpfile.babel.js
@@ -0,0 +1,149 @@
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import size from 'gulp-size';
+import plumber from 'gulp-plumber';
+import notify from 'gulp-notify';
+import beeper from 'beeper';
+import sass from 'gulp-sass';
+import sourcemaps from 'gulp-sourcemaps';
+import autoprefixer from 'autoprefixer';
+import postcss from 'gulp-postcss';
+import csscomb from 'gulp-csscomb';
+import gulpStylelint from 'gulp-stylelint';
+import rename from 'gulp-rename';
+import browserSync from 'browser-sync';
+
+const browserSyncServer = browserSync.create();
+
+/**
+ * Folder and file path config.
+ * Make edits here to change source
+ * or destination folder paths
+ *
+*/
+const config = {
+  src: {
+    sass: './sass/**/*.scss',
+    js: './js/*.js',
+    jsPolyfill: 'node_modules/babel-polyfill/dist/polyfill.js',
+    php: '../**/*.php',
+    htmlPrototypes: './example-layouts/**.*'
+  },
+  dist: {
+    cssFolder: '../',
+    js: '../js/'
+  }
+}
+
+/**
+ * Compile SCSS into CSS & auto-inject css
+ * into browsers if browserSync is running.
+ * This task also runs autoprefixer
+ * and CSS comb before outputting style.css
+*/
+function sassCompile() {
+  return gulp.src(config.src.sass)
+    .pipe(plumber({ errorHandler: function(err) {
+      notify.onError({
+          title: "Sass compile error in " + err.plugin,
+          message:  err.toString()
+      })(err);
+      // Play terminal beep to notify that error occured
+      beeper();
+    }}))
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([autoprefixer() ]))
+    .pipe(csscomb())
+    .pipe(rename('style.css'))
+    .pipe(size({showFiles : true}))
+    .pipe(sourcemaps.write('./src/sourcemaps'))
+    .pipe(gulp.dest(config.dist.cssFolder))
+};
+
+/**
+ * Compile JS
+ * TODO: Add minification step - not needed yet while prototyping
+ * // .pipe(uglify())
+ * // .pipe(concat('index.min.js'))
+*/
+function jsCompile() {
+  return gulp.src(
+    [
+      config.src.jsPolyfill,
+      config.src.js,
+    ], { sourcemaps: true })
+    .pipe(plumber({ errorHandler: function(err) {
+      notify.onError({
+          title: "JS compile error in " + err.plugin,
+          message:  err.toString()
+      })(err);
+      // Play terminal beep to notify that error occured
+      beeper();
+    }}))
+    .pipe(babel())
+    .pipe(rename('app.bundle.js'))
+    .pipe(gulp.dest(config.dist.js));
+}
+
+/**
+ * Intialise browserSync server to enable live reloading in browser
+ */
+function liveReloadServer(done) {
+  browserSyncServer.init({
+    proxy: {
+      target: 'http://localhost'
+    },
+    open: 'local',  // open localhost in browser on start
+    reloadDebounce: 500
+  });
+  done();
+}
+
+/**
+ * Trigger full browser reload
+ */
+function fullBrowserReload(done) {
+  browserSyncServer.reload();
+  done();
+}
+
+/**
+ * Inject changes into page without full reload
+ */
+function streamReload(done) {
+  browserSyncServer.reload({stream: true})
+  done();
+}
+
+/**
+ * Watches front end assets sass/js for changes, compiles & reload on change
+ * Also watches wordpress php files, reloads browser on change
+ */
+const watch = () => {
+  // If change detected to sass, recompile and auto inject updated css
+  gulp.watch(config.src.sass, gulp.series(sassCompile, fullBrowserReload))
+
+  // Watch JS for changes, recompile and trigger reload
+  gulp.watch(config.src.js, gulp.series(jsCompile, fullBrowserReload))
+
+  // Watch html prototypes for changes
+  gulp.watch(config.src.htmlPrototypes, gulp.series(sassCompile, fullBrowserReload))
+
+  // If change detected to php pages, trigger browser reload
+  gulp.watch(config.src.php, fullBrowserReload)
+};
+
+
+export {
+  sassCompile,
+  jsCompile
+}
+
+/**
+ * Local dev server task - compile and watch scss/php files for changes
+ * This task has a dependancy that the WP docker container is already running
+ * on localhost:80 for the livereload task to function
+ */
+const dev = gulp.series(sassCompile, jsCompile, liveReloadServer, watch);
+export default dev;
