c52a63281c9903d4db56e62801f8860c0e336e1c
Andrew Tarry <andrew.tarry@6point6.co.uk>
2018-10-21 12:53:09 +0100

    moved theme

---
+++ website-content/wp-content/themes/jsarc/template-parts/content.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package JSaRC
+ */
+
+?>
+
+<article <?php post_class(); ?>>
+	<header>
+		<?php
+		if ( is_singular() ) :
+			the_title( '<h1>', '</h1>' );
+		else :
+			the_title( '<h2><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif;
+
+		if ( 'post' === get_post_type() ) :
+			?>
+			<div>
+				<?php
+				jsarc_posted_on();
+				jsarc_posted_by();
+				?>
+			</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<?php jsarc_post_thumbnail(); ?>
+
+	<div>
+		<?php
+		the_content( sprintf(
+			wp_kses(
+				/* translators: %s: Name of current post. Only visible to screen readers */
+				__( 'Continue reading<span> "%s"</span>', 'jsarc' ),
+				array(
+					'span' => array(
+						'class' => array(),
+					),
+				)
+			),
+			get_the_title()
+		) );
+
+		wp_link_pages( array(
+			'before' => '<div>' . esc_html__( 'Pages:', 'jsarc' ),
+			'after'  => '</div>',
+		) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer>
+		<?php jsarc_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
