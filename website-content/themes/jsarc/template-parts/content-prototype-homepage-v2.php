12efae79037b8ea058dc22df398bc69f1424839f
Max Dmitriev <max.dmitriev@6point6.co.uk>
2018-10-23 11:32:38 +0100

    test commit

---
+++ website-content/themes/jsarc/template-parts/content-prototype-homepage-v2.php
@@ -0,0 +1,228 @@
+<style>
+</style>
+<h1 style="color:red">HOMEPAGE V2</h1>
+<section class="section section-hero">
+  <figure class="section-background-image"></figure>
+  <div class="content-wrapper">
+    <div class="section-content">
+      <div class="row">
+        <div class="column large-12 medium-7 small-12 small-centered">
+          <h1 class="section-headline">Securing crowded spaces </h1>
+          <p class="intro">Providing visitors with easy access and security. Screening large numbers of people quickly, cost-effectively and proportionately.</p>
+          <a class="button more" href="/case-study">Read the case study</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="section section-about-jsarc">
+  <div class="section-content">
+    <div class="viewport-content">
+      <div class="row">
+        <div class="column large-12 medium-7 small-12 small-offset-0 small-centered">
+          <h2 class="section-headline">Joint Security and Resilience Centre</h2>
+          <p class="about-jsarc-text">The Joint Security and Resilience Centre (JSaRC) is a government department that brings together government representatives, members of academia and the security sector to form a joint response to national security challenges.</p>
+          <p class="about-jsarc-text">Drawing on a wide pool of expertise, JSaRC works collaboratively with these diverse specialists to encourage innovation and to grow future opportunities in the security sector. 
+</p>
+          <a class="button more" href="#">Find out more <span class="u-hide-sm-only">about working with JSaRC</span></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="section section-video">
+  <div class="section-content">
+    <figure class="section-background-image"></figure>
+    <div class="row">
+      <div class="column large-offset-6 large-6 medium-6 small-12 small-centered">
+        <h2 class="section-headline">How JSaRC is changing the face of National Security</h2>
+        <p class="speaker">Shaun Hipgrave, Head of JSaRC</p>
+        <a id="videoOverlayTrigger" class="button more" href="#">Watch Video</a>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="section section-benefits">
+  <div class="section-content">
+    <h2 class="section-headline">Benefits of engaging with JSaRC</h2>
+      <div class="media-block">
+        <figure class="benefits-image-1"></figure>
+        <div class="media-block__text-content">
+          <h3 class="media-block__title">Partnerships</h3>
+          <p class="media-block__text">JSaRC helps the UK government, academia and suppliers from the security sector to work closely together and forge new partnerships. It is an effective and efficient way to collaborate, share ideas and make contacts. JSaRC enables people and organisations to work together to meet the security challenges and issues facing Great Britain today.
+          <a class="media-block__read-more-link">Read more</a>
+        </div>
+      </div>
+
+      <div class="row media-block media-block--image-left">
+        <figure class="benefits-image-2"></figure>
+        <div class="media-block__text-content">
+          <h3 class="media-block__title">Funding Opportunities</h3>
+          <p class="media-block__text">Working with JSaRC, you will broaden your connections, gain valuable exposure and contribute to ongoing security sector development with Her Majesty’s Government. You can gain access to available funding opportunities. JSaRC also provides support with applications for funding made by interested members of the security sector.
+          <a class="media-block__read-more-link">Read more</a>
+        </div>
+      </div>
+  </div>
+</section>
+
+
+<section class="section section-contact-us">
+  <div class="section-content">
+      <div class="row">
+        <div class="column large-5 medium-6 small-12 ">
+          <p class="contact-us-text">If you are a member of industry, an academic or someone in government with a concept or project you think can contribute to the security of the nation, JSaRC wants to hear from you.
+        </div>
+        <div class="column large-4 large-offset-3 medium-4 medium-offset-1 small-12 small-offset-0">
+          <a class="button more button-contact-us">Contact Us</a>
+        </div>
+      </div>
+  </div>
+</section>
+
+<section class="section section-innovation-call">
+  <div class="section-content">
+    <figure class="section-background-image"></figure>
+    <div class="row">
+      <div class="column large-7 medium-7 small-12 small-offset-0 small-centered">
+        <h2 class="section-headline">Innovation call</h2>
+        <p class="about-jsarc-text">You can tell JSaRC about a product or service you can provide to the government, or you can respond to an Innovation Call.</p>
+        <p class="about-jsarc-text">JSaRC makes Innovation Calls for engagement from people and organisations and we welcome people’s responses. You can look at JSaRC’s current opportunities and register your interest in updates and events.</p>
+        <a class="button more" href="#">Contact JSaRC</a>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="section section-case-study">
+  <div class="section-content">
+    <figure class="section-background-image"></figure>
+    <div class="row">
+      <div class="column large-8 medium-6 small-12 small-centered">
+        <h2 class="section-headline">JSaRC Projects and Case Studies</h2>
+        <p class="section-sub-heading">High Footfall Screening Trials</p>
+        <a class="button more" href="/case-study">Read the case study</a>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+<section class="section section-projects">
+  <div class="section-content">
+    <div class="row">
+      <ul class="projects-3up">
+        <li class="projects-3up__item">
+          <div class="projects-3up__image-wrapper">
+            <a href="/case-study">
+              <figure class="projects-3up__image" style="background-image: url('wp-content/themes/jsarc/img/v/homepage/a/project-thumb1.png'"></figure>
+            </a>
+          </div>
+          <div class="projects-3up__content-wrapper">
+            <h3 class="projects-3up__heading">Explosive Detection Technology Review (EDTR)</h3>
+            <p class="projects-3up__text">
+              JSaRC worked with the security sector and academics to review the current position and availability of explosives detection technology in the UK.</p>
+            <a href="/case-study" class="projects-3up__read-more-link">
+              Read the case study
+            </a>
+          </div>
+        </li>
+        <li class="projects-3up__item">
+            <div class="projects-3up__image-wrapper">
+              <a href="/case-study">
+                <figure class="projects-3up__image"  style="background-image: url('wp-content/themes/jsarc/img/v/homepage/a/project-thumb2.png'"></figure>
+              </a>
+            </div>
+            <div class="projects-3up__content-wrapper">
+              <h3 class="projects-3up__heading">Joint Overseas Protect and Prepare (JOPP)</h3>
+              <p class="projects-3up__text">
+                The JOPP programme is reducing the ongoing risk from terrorism at home and abroad. Security and protection remain our top priorities.</p>
+              <a href="/case-study" class="projects-3up__read-more-link">
+                Read the case study
+              </a>
+            </div>
+        </li>
+        <li class="projects-3up__item">
+          <div class="projects-3up__image-wrapper">
+            <a href="/case-study">
+              <figure class="projects-3up__image" style="background-image: url('wp-content/themes/jsarc/img/v/homepage/a/project-thumb3.png'"></figure>
+            </a>
+          </div>
+          <div class="projects-3up__content-wrapper">
+            <h3 class="projects-3up__heading">Passengers and borders</h3>
+            <p class="projects-3up__text">
+            The volume of border crossings is addressed in JSaRC's coaches, cars and bottlenecks review.</p>
+            <a href="/case-study" class="projects-3up__read-more-link">
+              Read the case study
+            </a>
+          </div>
+        </li>
+      </ul>
+    </div>
+
+    <div class="row">
+      <a href="#" class="button more see-all-projects-button">Find out more about JSaRC projects</a>
+    </div>
+  </div>
+
+</section>
+
+
+<section class="section section-register">
+  <div class="section-content">
+    <div class="row register">
+      <div class="column large-4 small-12">
+        <h3 class="headline">Register for a JSaRC Open Day or event<h3>
+      </div>
+      <div class="column large-7 small-12">
+        <form id="register" class="register-form" action="" method="post">
+              <div class="register-input">
+                <input id="register-input" class="register-input__text-field" type="text" name="register-input__input">
+                <label class="register-input__label" for="register-input">Enter your email to receive information</label>
+              </div>
+          <input type="submit" class="button register-submit-button" value="submit" onclick="" />
+        </form>
+      </div>
+    </div>
+    <div class="row social">
+      <div class="column large-4 small-12">
+        <h3 class="headline">Stay connected with JSaRC</h3>
+      </div>
+      <div class="column large-7 small-12">
+        <ul class="social-icons">
+          <li class="menu-item"><a class="icon twitter" href="#">Twitter</a></li>
+          <li class="menu-item"><a class="icon youtube" href="#">Youtube</a></li>
+          <li class="menu-item"><a class="icon facebook" href="#">Facebook</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+
+<script>
+  // Register for open days email input
+  // (animated floating label style text input)
+  var registerTextInput = document.querySelector('.register-input__text-field');
+
+  if (registerTextInput) {
+
+    registerTextInput.addEventListener('change', function() {
+
+      // When text input field is changed, if it contains content add 'active' class
+      // To ensure input label remains floated above the input if text has been added
+      if (registerTextInput && registerTextInput.value) {
+        registerTextInput.classList.add('active');
+      }
+      else {
+        registerTextInput.classList.remove('active');
+      }
+    });
+  }
+</script>
