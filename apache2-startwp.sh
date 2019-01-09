2625af6e32d144e86d8f91b38870576470a943fa
maltamash <moh-alt@hotmail.co.uk>
2019-01-09 16:24:14 +0000

    removing plugins

--- apache2-startwp.sh
+++ apache2-startwp.sh
@@ -6,11 +6,6 @@ wp plugin install $1 --activate
 
 cp -r /var/www/themes/jsarc /var/www/html/wp-content/themes/
 
-wp_plugin_install wordpress-importer
-wp_plugin_install salesforce-wordpress-to-lead
-wp_plugin_install simply-static
-wp_plugin_install advanced-custom-fields-pro
-wp_plugin_install acf-image-crop-add-on
 
 
 if [ -d "/var/www/jsarc" ] ; then
