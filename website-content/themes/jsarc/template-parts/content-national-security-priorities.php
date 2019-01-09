4c989b33ad2a45822c1768847ddfb82f19997ca0
Max Dmitriev <max.dmitriev@6point6.co.uk>
2019-01-09 16:13:33 +0000

    nation security priorities page added

--- website-content/themes/jsarc/template-parts/content-national-security-priorities.php
+++ website-content/themes/jsarc/template-parts/content-national-security-priorities.php
@@ -9,7 +9,8 @@ get_header();
 
 <style>
 
-.article-header .hero-image {
+
+.hero .hero-image {
 	position: absolute;
 	z-index: -1;
 	max-width: 2560px;
@@ -17,8 +18,239 @@ get_header();
 	height: 300px;
 	background-size: cover;
 	background-position: 80% 50%;
-	background-image: url("<?php if ( get_field( 'hero_image' ) ) { the_field( 'hero_image' ); } ?>");
+	background-image: url("<?php if ( get_field( 'hero_image' ) ) { the_field( 'hero_image' ); } ?>");	
 }
+.hero .section-content {
+	display: table;
+	height: 300px;
+	
+}
+.hero .headline-wrapper {
+	display: table-cell;
+	vertical-align: middle;
+}
+
+.hero .hero-headline {
+	display: inline-block;
+	width: 80%;
+	font-size: 36px;
+	line-height: 75px;
+	font-weight: bold;
+	color: #fff;
+}
+.hero .hero-headline span {
+	display: inline;
+	padding: 2px 0;
+	box-shadow: 5px 0 0 #000, -5px 0 0 #000; 
+	background-color: #000;
+}
+
+
+
+.section.heading {
+	background-color: #fff;
+	margin-bottom: 50px;
+}
+.section.heading .section-content {
+	padding-top: 75px;
+	padding-bottom: 75px;
+}
+.section.heading .hero-heading-text {
+	color: #4A4A4A;
+	font-size: 22px;
+	line-height: 34px;
+	font-weight: bold;
+	margin-bottom: 50px;
+}
+.section.heading .hero-body-text {
+	color: #4A4A4A;
+	font-size: 18px;
+	line-height: 28px
+}
+.section.heading .hero-body-text strong {
+	 color: #000;
+}
+.section.heading .hero-body-text p {
+	margin-bottom: 1em;
+}
+.section.heading .hero-body-text p:last-child {
+	margin-bottom: 0;
+}
+
+
+
+
+
+
+.sections-wrapper {
+	background-color: #E3E3E3;
+	padding: 50px 0;
+}
+.section.priority {
+	margin-bottom: 50px;
+}
+.section.priority .section-content {
+	
+}
+@media only screen and (min-width: 736px) {
+	.section.priority.image-left .row > .column.large-last {
+	 float: left;
+	}
+	.section.priority.image-left .row > .column.large-first {
+	 float: right;
+	}
+}
+
+.section.priority .section-image {
+	height: 600px;
+	width: 100%;
+	background-size: cover;
+	background-position: 50% 50%;
+}
+
+
+.section.priority .block {
+	background-color: #fff;
+	
+}
+.section.priority .block-text-content {
+	display: table;
+	padding: 50px;
+	min-height: 500px;
+}
+@media only screen and (max-width: 735px) {
+	.section.priority .section-image {
+		height: 400px;
+	}
+	.section.priority .block-text-content {
+		padding: 50px;
+		min-height: 300px;
+	}
+}
+
+.section.priority .block-text-inner {
+	display: table-cell;
+	vertical-align: middle;
+}
+.section.priority .block-headline {
+	font-size: 24px;
+	font-weight: bold;
+	line-height: 34px;
+	margin-bottom: 22px;
+}
+.section.priority .block-text {
+	font-size: 18px;
+	line-height: 28px;
+	margin-bottom: 50px;
+}
+.section.priority .block-text p {
+	margin-bottom: 1em;
+}
+.section.priority .block-link {
+	color: #01749F;
+	font-size: 19px;
+	font-style: oblique;
+	line-height: 28px;
+}
+
+
+.section.engaging {
+	background-color: #1E4289;
+	overflow: hidden;
+	position: relative;
+}
+
+.section.engaging:before {
+	background-color: #fff;
+	content: '';
+	display: block;
+	position: absolute;
+	width: 50%;
+	right: 0;
+	height: 100%;
+}
+
+
+
+
+
+.section.engaging .section-content {
+	padding-top: 70px;
+	padding-bottom: 70px;
+}
+
+
+.section.engaging .blue .column-content {
+	color: #fff;
+	padding-right: 50px;
+}
+
+.section.engaging .white .column-content {
+	padding-left: 50px;
+}
+
+@media only screen and (max-width: 735px) {
+	.section.engaging .section-content {
+		width: 100%;
+		padding-bottom: 0;
+	}
+	.section.engaging:before {
+		display: none;
+	}
+	 
+	.section.engaging .blue .column-content {
+		padding-bottom: 50px;
+		padding-right: 0;
+		width: 90%;
+		margin-left: auto;
+		margin-right: auto;
+	}
+	.section.engaging .white {
+		background-color: #fff;
+	}
+	.section.engaging .white .column-content {
+		padding-top: 50px;
+		padding-bottom: 75px;
+		padding-left: 0;
+		width: 90%;
+			margin-left: auto;
+			margin-right: auto;
+	}
+	 
+}
+
+
+.section.engaging .section-headline {
+	font-size: 36px;
+	line-height: 53px;
+	margin-bottom: 30px;
+}
+
+.section.engaging .section-body-text {
+	margin-bottom: 50px;
+}
+
+.section.engaging .section-body-text > *,
+.section.engaging .body-text-second-column > * {
+	font-size: 18px;
+	line-height: 28px;
+	margin-bottom: 1em;
+}
+.section.engaging .section-body-text > *:last-child,
+.section.engaging .body-text-second-column > *:last-child {
+	margin-bottom: 0;
+}
+
+.section.engaging .body-text-second-column {
+	color: #4A4A4A;
+}
+
+.section.engaging .button.more {
+	border: 2px solid #fff;
+	width: 100%;
+	font-size: 19px;
+}
+
 
 
 
@@ -27,6 +259,15 @@ get_header();
 
 
 
+
+	<header class="section hero">
+		<figure class="hero-image"></figure>
+		<div class="section-content">
+			<div class="headline-wrapper">
+				<h1 class="hero-headline"><span><?php the_title(); ?></span></h1>
+			</div>
+		</div>
+	</header>
 	<nav class="breadcrumbs">
 		<div class="section-content">
 			<ul class="breadcrumbs-list">
@@ -35,66 +276,125 @@ get_header();
 			</ul>
 		</div>
 	</nav>
-	<header class="section hero">
+	<section class="section heading">
 		<div class="section-content">
-			<?php the_field( 'hero_heading_text' ); ?>
-			<?php the_field( 'hero_body_text' ); ?>
-		</div>
-	</header>
-	
-	
-	<?php if ( have_rows( 'sections' ) ) : ?>
-		<?php while ( have_rows( 'sections' ) ) : the_row(); ?>
-		
-		<?php if ( get_sub_field( 'image_alignment_' ) == 1 ) {  // echo 'true';  ?>
-			
-			 <section class="section box image-right">
-			<?php } else { // echo 'false';   ?>
-			 
-			  <section class="section box image-left">
-			<?php } ?>
-		
- 
-		<div class="section-content">
-		<div>
-			<?php if ( get_sub_field( 'section_image' ) ) { ?>
-				<figure style="background-image: url(<?php the_sub_field( 'section_image' ); ?>)"></figure>
-			<?php } ?>
-		</div>
-		<div>	
-			<?php if ( have_rows( 'section' ) ) : ?>
-				<?php while ( have_rows( 'section' ) ) : the_row(); ?>
-					<?php the_sub_field( 'section_headline' ); ?>
-				<?php endwhile; ?>
-			<?php endif; ?>
-			<?php the_sub_field( 'section_body_text' ); ?>
-			<?php if ( have_rows( 'section_links' ) ) : ?>
-				<?php while ( have_rows( 'section_links' ) ) : the_row(); ?>
-					<?php if ( have_rows( 'section_link' ) ) : ?>
-						<?php while ( have_rows( 'section_link' ) ) : the_row(); ?>
-							<?php the_sub_field( 'section_link_text' ); ?>
-							<?php the_sub_field( 'section_link_url' ); ?>
-						<?php endwhile; ?>
-					<?php endif; ?>
-				<?php endwhile; ?>
-			<?php else : ?>
-				<?php // no rows found ?>
-			<?php endif; ?>
-		<?php endwhile; ?>
-		</div>
+			<h3 class="hero-heading-text"><?php the_field( 'hero_heading_text' ); ?></h3>
+			<div class="hero-body-text"><?php the_field( 'hero_body_text' ); ?></div>
 		</div>
 	</section>
-	<?php else : ?>
-		<?php // no rows found ?>
-	<?php endif; ?>
 	
+<div class="sections-wrapper">	
+	<?php if ( have_rows( 'sections' ) ) : ?>
+<?php while ( have_rows( 'sections' ) ) : the_row(); ?>
 
+<section class="section priority image-<?php the_sub_field( 'image_alignment_' ); ?>">
+	<div class="section-content">
+		<div class="block row">
+			<div class="column large-5 large-last small-12">
+<?php if ( get_sub_field( 'section_image' ) ) { ?>
+				<figure class="section-image" style="background-image: url(<?php the_sub_field( 'section_image' ); ?>)">
+				</figure>
+<?php } ?>
+			</div>
+			<div class="column large-7 large-first small-12">
+				<div class="block-text-content">
+					<div class="block-text-inner">
+						<h3 class="block-headline">
+<?php if ( have_rows( 'section' ) ) : ?>
+<?php while ( have_rows( 'section' ) ) : the_row(); ?>
+<?php the_sub_field( 'section_headline' ); ?>
+<?php endwhile; ?>
+<?php endif; ?>
+						</h3>
+						<div class="block-text">
+<?php the_sub_field( 'section_body_text' ); ?>
+						</div>
+						 
+<?php if ( have_rows( 'section_links' ) ) : ?>
+			<?php while ( have_rows( 'section_links' ) ) : the_row(); ?>
+				<?php if ( have_rows( 'section_link' ) ) : ?>
+					<?php while ( have_rows( 'section_link' ) ) : the_row(); ?>
+						<div><a class="block-link" href="<?php the_sub_field( 'section_link_url' ); ?>"><?php the_sub_field( 'section_link_text' ); ?>
+						</a></div>
+<?php endwhile; ?>
+				<?php endif; ?>
+			<?php endwhile; ?>
+		<?php else : ?>
+			<?php // no rows found ?>
+		<?php endif; ?>
+	 
+				</div>
+			</div>
+		</div>
+		
+		
+					
+		</div>
+	</section>			
+			
+		
+		
+	<?php endwhile; ?>
+<?php else : ?>
+	<?php // no rows found ?>
+<?php endif; ?>
+	</div>				
+		
+					
+			
+		
+		
+		
+		 
+		
+				
+	
+	
+		
+		
+		
+		
+		
+		
+		<?php if ( have_rows( 'section_engaging' ) ) : ?>
+	<?php while ( have_rows( 'section_engaging' ) ) : the_row(); ?>
 	
 	<section class="section engaging">
 		<div class="section-content">
-			
+	<div class="row">
+	<div class="column large-6 small-12 blue">
+	<div class="column-content">
+		<h3 class="section-headline"><?php the_sub_field( 'section_headline_' ); ?></h3>
+		<div class="section-body-text"><?php the_sub_field( 'section_body_text' ); ?></div>
+		<?php if ( have_rows( 'section_button' ) ) : ?>
+			<?php while ( have_rows( 'section_button' ) ) : the_row(); ?>
+				<a class="button more" href="<?php the_sub_field( 'button_url' ); ?>"><?php the_sub_field( 'button_text' ); ?></a>
+				
+			<?php endwhile; ?>
+		<?php endif; ?>
 		</div>
+		</div>
+		<div class="column large-6 small-12 white">
+			<div class="column-content">
+				<div class="body-text-second-column">
+					<?php the_sub_field( 'section_body_text_second_column' ); ?>
+				</div>
+			</div>
+		</div>
+			</div>
 	</section>
+	<?php endwhile; ?>
+<?php endif; ?>
+		
+		
+		
+		
+		
+		
+		
+		
+			
+	
 
 <?php get_template_part( 'template-parts/content', 'section-register'); ?>
 <?php
