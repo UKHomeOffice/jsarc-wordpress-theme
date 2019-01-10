07c3959f7017fe760aa9589c6aa02cac4fcbb294
maltamash <moh-alt@hotmail.co.uk>
2019-01-10 16:32:46 +0000

    testing again

--- apache2-startwp.sh
+++ apache2-startwp.sh
@@ -7,6 +7,7 @@ www-data -s /bin/sh -c "wp plugin install $1 --activate"
 
 cp -r /var/www/themes/jsarc /var/www/html/wp-content/themes/
 cp -r /var/www/html/wp-content/uploads /var/www/temp
+cp -r /var/www/temp /var/www/html/wp-content/uploads 
 
 
 
