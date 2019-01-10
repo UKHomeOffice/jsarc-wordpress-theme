37d2be93a4d669c378f032efe6ca7d24fa79b431
maltamash <moh-alt@hotmail.co.uk>
2019-01-10 16:16:21 +0000

    removing folders

--- apache2-startwp.sh
+++ apache2-startwp.sh
@@ -7,6 +7,10 @@ www-data -s /bin/sh -c "wp plugin install $1 --activate"
 
 cp -r /var/www/themes/jsarc /var/www/html/wp-content/themes/
 
+rm -rf /var/www/html/wp-content/plugins/
+rm -rf /var/www/html/wp-content/upgrade/
+rm -rf /var/www/html/wp-content/uploads/
+
 wp_plugin_install wordpress-importer
 wp_plugin_install salesforce-wordpress-to-lead
 wp_plugin_install simply-static
