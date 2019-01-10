17fa42c7b067fd5498826814ec998efa42d89a0c
maltamash <moh-alt@hotmail.co.uk>
2019-01-10 15:01:21 +0000

    changing

--- apache2-startwp.sh
+++ apache2-startwp.sh
@@ -10,7 +10,7 @@ cp -r /var/www/themes/jsarc /var/www/html/wp-content/themes/
 wp_plugin_install wordpress-importer
 wp_plugin_install salesforce-wordpress-to-lead
 wp_plugin_install simply-static
-wp_plugin_install acf-theme-code-pro
+wp_plugin_install acf_theme_code_pro
 wp_plugin_install acf-image-crop-add-on
 wp_plugin_install advanced-custom-fields-pro
 
