7e3556a72b428163e56c8eef81901009e3455b1c
maltamash <moh-alt@hotmail.co.uk>
2019-01-10 20:38:36 +0000

    modifying

--- apache2-startwp.sh
+++ apache2-startwp.sh
@@ -8,8 +8,8 @@ www-data -s /bin/sh -c "wp plugin install $1 --activate"
 
 cp -r /var/www/themes/jsarc /var/www/html/wp-content/themes/
 
-mkdir -p /var/www/media 
-echo "define( 'UPLOADS', '/var/www/media');" >> /var/www/html/wp-config.php
+mkdir -p /var/www/html/media/
+echo "define( 'UPLOADS', '/var/www/html/media');" >> /var/www/html/wp-config.php
 
 
 
@@ -27,7 +27,6 @@ rm -rf /var/www/html/wp-content/themes/jsarc
 ln -s /var/www/jsarc /var/www/html/wp-content/themes/jsarc
 fi
 
-cp -r /var/www/temp /var/www/html/wp-content/uploads
 
 
 $@
