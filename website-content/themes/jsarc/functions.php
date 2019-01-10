79982d0c7ffbe8653b852af191da9552e9edc23b
Max Dmitriev <max.dmitriev@6point6.co.uk>
2019-01-10 10:51:35 +0000

    acf inluded in theme

--- website-content/themes/jsarc/functions.php
+++ website-content/themes/jsarc/functions.php
@@ -398,14 +398,52 @@ add_filter('nav_menu_css_class', 'remove_css_id_filter', 100, 1);
 add_theme_support( 'post-thumbnails' );
  
 
+
+
+
+
+
+
+
+
+
+
+
+// 1. customize ACF path
+add_filter('advanced-custom-fields-pro/settings/path', 'my_acf_settings_path');
  
+function my_acf_settings_path( $path ) {
  
+    // update path
+    $path = get_stylesheet_directory() . '/advanced-custom-fields-pro/';
+    
+    // return
+    return $path;
+    
+}
  
+
+// 2. customize ACF dir
+add_filter('advanced-custom-fields-pro/settings/dir', 'my_acf_settings_dir');
  
+function my_acf_settings_dir( $dir ) {
  
+    // update path
+    $dir = get_stylesheet_directory_uri() . '/advanced-custom-fields-pro/';
+    
+    // return
+    return $dir;
+    
+}
  
- 
- 
+
+// 3. Hide ACF field group menu item
+add_filter('advanced-custom-fields-pro/settings/show_admin', '__return_false');
+
+
+// 4. Include ACF
+include_once( get_stylesheet_directory() . '/advanced-custom-fields-pro/acf.php' );
+
  
  
  
