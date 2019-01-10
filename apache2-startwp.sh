b4beb025f815f3ec943e3c99a980cfa574fa8cb1
maltamash <moh-alt@hotmail.co.uk>
2019-01-10 15:26:57 +0000

    adding plugins back

--- apache2-startwp.sh
+++ apache2-startwp.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-#function wp_plugin_install() {
-#www-data -s /bin/sh -c "wp plugin install $1 --activate"
+function wp_plugin_install() {
+www-data -s /bin/sh -c "wp plugin install $1 --activate"
 
-#}
+}
 
 cp -r /var/www/themes/jsarc /var/www/html/wp-content/themes/
 
-#wp_plugin_install wordpress-importer
-#wp_plugin_install salesforce-wordpress-to-lead
-#wp_plugin_install simply-static
-#wp_plugin_install acf_theme_code_pro
-#wp_plugin_install acf-image-crop-add-on
-#wp_plugin_install advanced-custom-fields-pro
+wp_plugin_install wordpress-importer
+wp_plugin_install salesforce-wordpress-to-lead
+wp_plugin_install simply-static
+wp_plugin_install acf-theme-code-pro
+wp_plugin_install acf-image-crop-add-on
+wp_plugin_install advanced-custom-fields-pro
 
 if [ -d "/var/www/jsarc" ] ; then
 rm -rf /var/www/html/wp-content/themes/jsarc
