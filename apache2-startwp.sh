e09d2889a5cdbf040d390a3f13ea862c483c74c8
maltamash <moh-alt@hotmail.co.uk>
2019-01-10 16:45:27 +0000

    trying something

--- apache2-startwp.sh
+++ apache2-startwp.sh
@@ -7,7 +7,7 @@ www-data -s /bin/sh -c "wp plugin install $1 --activate"
 
 cp -r /var/www/themes/jsarc /var/www/html/wp-content/themes/
 cp -r /var/www/html/wp-content/uploads /var/www/temp
-cp -r /var/www/temp /var/www/html/wp-content/uploads 
+
 
 
 
@@ -22,5 +22,8 @@ if [ -d "/var/www/jsarc" ] ; then
 rm -rf /var/www/html/wp-content/themes/jsarc
 ln -s /var/www/jsarc /var/www/html/wp-content/themes/jsarc
 fi
+
+cp -r /var/www/temp /var/www/html/wp-content/uploads
+
 
 $@
