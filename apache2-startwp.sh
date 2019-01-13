15d91ec862e37b44c4d4c68ea764117f65279dec
Max Dmitriev <max.dmitriev@6point6.co.uk>
2019-01-13 18:57:52 +0000

    wp absolute path defined 8

--- apache2-startwp.sh
+++ apache2-startwp.sh
@@ -12,8 +12,9 @@ echo "define('WP_PLUGIN_DIR', '' . '../wp-content/themes/jsarc/plugins');" >> /v
 echo "define('WP_PLUGIN_URL', '' . '/wp-content/themes/jsarc/plugins');" >> /var/www/html/wp-includes/default-constants.php
 
 
-echo "add_filter( 'the_field', 'wp_make_link_relative');" >> /var/www/html/wp-includes/link-template.php
-echo "add_filter( 'the_sub_field', 'wp_make_link_relative');" >> /var/www/html/wp-includes/link-template.php
+echo "echo wp_make_link_relative('https://web.notprod.jsarc.homeoffice.gov.uk/');" >> /var/www/html/wp-includes/formatting.php
+echo "add_filter( 'the_sub_field', 'wp_make_link_relative');" >> /var/www/html/wp-includes/formatting.php
+echo "add_filter( 'the_field', 'wp_make_link_relative');" >> /var/www/html/wp-includes/formatting.php
 
 
 # wp_plugin_install wordpress-importer
