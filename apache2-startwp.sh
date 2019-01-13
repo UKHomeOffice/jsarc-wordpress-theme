046121f468ebbb4f35bc0d800cab1536616c3ca9
Max Dmitriev <max.dmitriev@6point6.co.uk>
2019-01-13 16:14:21 +0000

    wp absolute path defined 3

--- apache2-startwp.sh
+++ apache2-startwp.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-function wp_plugin_install() {
-www-data -s /bin/sh -c "wp plugin install $1 --activate"
-
-}
+# function wp_plugin_install() {
+# www-data -s /bin/sh -c "wp plugin install $1 --activate"
+# }
 
 
 cp -r /var/www/themes/jsarc /var/www/html/wp-content/themes/
 
-echo "define('UPLOADS', '' . 'images');" >> /var/www/html/wp-config.php
+# echo "define('UPLOADS', '' . 'images');" >> /var/www/html/wp-config.php
 echo "define('WP_PLUGIN_DIR', '' . '../wp-content/themes/jsarc/plugins');" >> /var/www/html/wp-includes/default-constants.php
+echo "define('WP_PLUGIN_URL', '' . '../wp-content/themes/jsarc/plugins');" >> /var/www/html/wp-includes/default-constants.php
 
 
-wp_plugin_install wordpress-importer
-wp_plugin_install salesforce-wordpress-to-lead
-wp_plugin_install simply-static
-wp_plugin_install acf-theme-code-pro
-wp_plugin_install acf-image-crop-add-on
-wp_plugin_install advanced-custom-fields-pro
+# wp_plugin_install wordpress-importer
+# wp_plugin_install salesforce-wordpress-to-lead
+# wp_plugin_install simply-static
+# wp_plugin_install acf-theme-code-pro
+# wp_plugin_install acf-image-crop-add-on
+# wp_plugin_install advanced-custom-fields-pro
 
 if [ -d "/var/www/jsarc" ] ; then
 rm -rf /var/www/html/wp-content/themes/jsarc
