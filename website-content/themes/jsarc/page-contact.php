0c14a940aa35822d69588834b2b21fa39cbbc458
Max Dmitriev <max.dmitriev@6point6.co.uk>
2019-01-16 15:56:51 +0000

    contact page template

---
+++ website-content/themes/jsarc/page-contact.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Template Name: Contact Page Template
+ * The template for displaying About page
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package JSaRC
+ */
+?>
+
+
+<?php get_header(); ?>
+
+
+<style>
+
+</style>
+
+
+<section class="section">
+	<div class="section-content">
+	
+	</div>
+</section>
+
+
+<?php get_footer();
