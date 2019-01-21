985c5d7dcd0f3be15a38aaeababff131602fd373
Max Dmitriev <max.dmitriev@6point6.co.uk>
2019-01-21 14:55:03 +0000

    contact form 2

--- website-content/themes/jsarc/contact_form.php
+++ website-content/themes/jsarc/contact_form.php
@@ -5,23 +5,27 @@ if( isset($_POST['message']) && isset($_POST['name']) && isset($_POST['email']) 
 	$email = $_POST['email'];
 	$phone = $_POST['phone'];
 	$organisation = $_POST['organisation'];
-	$to = "max.dmitriev@6point6.co.uk";	
+	$to = 'max.dmitriev@6point6.co.uk';	
 	$from = $email;
 	$subject = 'Contact Form Message';
-	$message = '<b>Name:</b> '.$name.' <br>
-				<b>Email:</b> '.$email.' <br>
-				<b>Telephone:</b> '.$phone.' <br>
-				<b>Organisation:</b> '.$organisation.' <br>
-				<b>Message:</b> '.$message;
-				
-	$headers = "From: $from\n";
-	$headers .= "MIME-Version: 1.0\n";
-	$headers .= "Content-type: text/html; charset=iso-8859-1\n";
-	if( mail($to, $subject, $message, $headers) ){
-		echo "success";
+	$content = '<b>Name:</b> '.$name.' <br> <b>Email:</b> '.
+	$email.' <br> <b>Telephone:</b> '.
+	$phone.' <br> <b>Organisation:</b> '.
+	$organisation.' <br> <b>Message:</b> '.$message;
+	
+	$headers = 'From:' . $from . '\r\n' .
+	'MIME-Version: 1.0' . '\r\n' .
+	'Content-type: text/html; charset=UTF-8;' . '\r\n';
+	
+	if( mail($to, $subject, $content, $headers) ){
+		echo 'success';
 	} else {
-		echo "The server failed to send the message. Please try again later.";
+		echo 'The server failed to send the message. Please try again later.';
 	}
 }
 ?>
+
+
+
+
 
