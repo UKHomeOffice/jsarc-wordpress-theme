5b56edf17b5b0c5e00d50b13f394f5d6a1b9b32b
Max Dmitriev <max.dmitriev@6point6.co.uk>
2019-01-22 16:12:59 +0000

    register section editable

--- website-content/themes/jsarc/template-parts/content-section-register.php
+++ website-content/themes/jsarc/template-parts/content-section-register.php
@@ -1,16 +1,30 @@
+
 <section class="section section-register">
 	<div class="section-content">
-		<h3 class="section-headline">Stay connected with JSaRC</h3>
-		<a class="button more" href="/register/">Register with JSaRC</a>
-		<p class="description">To receive the latest news and updates</p>
+		<h3 class="section-headline"><?php the_field( 'headline', 'option' ); ?></h3>
+		
+		
+		<?php if ( have_rows( 'button', 'option' ) ) : ?>
+			<?php while ( have_rows( 'button', 'option' ) ) : the_row(); ?>
+				<a class="button more" href="<?php the_sub_field( 'url' ); ?>"><?php the_sub_field( 'text' ); ?></a>
+			<?php endwhile; ?>
+		<?php endif; ?>
+		
+		<p class="description"><?php the_field( 'body_text', 'option' ); ?></p>
+		
+		<?php if ( have_rows( 'social', 'option' ) ) : ?>
 		<div class="social">
-			<h4 class="social-headline">Follow us on</h4>
-			<ul class="social-list">
-				<li class="social-item"><a class="social-link twitter" href="https://twitter.com/ukhomeoffice">Twitter</a></li>
-				<li class="social-item"><a class="social-link linkedin" href="https://www.linkedin.com/company/the-home-office/">LinkedIn</a></li>
-				<li class="social-item"><a class="social-link facebook" href="https://www.facebook.com/ukhomeofficegov/">LinkedIn</a></li>
-				<li class="social-item"><a class="social-link youtube" href="https://www.youtube.com/user/ukhomeoffice">YouTube</a></li>
-			</ul>
-		<div>
+			<?php while ( have_rows( 'social', 'option' ) ) : the_row(); ?>
+			<h4 class="social-headline"><?php the_sub_field( 'headline' ); ?></h4>
+				<?php if ( have_rows( 'social_links' ) ) : ?>
+				<ul class="social-list">
+					<?php while ( have_rows( 'social_links' ) ) : the_row(); ?>
+					<li class="social-item"><a class="social-link <?php the_sub_field( 'social_network' ); ?>" href="<?php the_sub_field( 'url' ); ?>"><?php the_sub_field( 'social_network' ); ?></a></li>
+					<?php endwhile; ?>
+				</ul>
+				<?php endif; ?>
+			<?php endwhile; ?>
+		</div>
+		<?php endif; ?>
 	</div>
 </section>
