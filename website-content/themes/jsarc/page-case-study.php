3a911944747022c53c02b8822388cd38548f53b3
Max Dmitriev <max.dmitriev@6point6.co.uk>
2019-01-25 16:35:46 +0000

    section case study added

--- website-content/themes/jsarc/page-case-study.php
+++ website-content/themes/jsarc/page-case-study.php
@@ -264,15 +264,6 @@ PAGE CASE STUDY / SECTION CASE-STUDY-CONTENT
 </section>
 
 
-<!--section class="section section-casestudies" data-anim-classname='{"cssClass":"reveal"}'>
-	<figure class="section-image-full-with"></figure>
-	<div class="section-content">
-		<h3 class="section-headline">JSaRC projects and case studies</h3>
-		<h4 class="section-sub-heading">High Footfall Screening Trials</h4>
-		<a class="button more" href="">Read the case study</a>
-	</div>
-</section-->
-
 <?php if ( have_rows( 'case_study_section' ) ) : ?>
 	<?php while ( have_rows( 'case_study_section' ) ) : the_row(); ?>
 	<section class="section section-casestudies" data-anim-classname='{"cssClass":"reveal"}'>
