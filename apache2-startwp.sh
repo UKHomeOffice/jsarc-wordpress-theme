6cb66a6620341bfb5462fed3dbab8cf8f6d5f295
Andrew Tarry <andrew.tarry@6point6.co.uk>
2019-01-29 11:59:12 +0000

    add config update

--- apache2-startwp.sh
+++ apache2-startwp.sh
@@ -9,7 +9,8 @@ cp -r /var/www/themes/jsarc /var/www/html/wp-content/themes/
 echo "$SITE_URL"
 echo "define('WP_HOME', '$SITE_URL');" >> /var/www/html/wp-includes/default-constants.php
 echo "define('WP_SITEURL', '$SITE_URL');" >> /var/www/html/wp-includes/default-constants.php
-cat /var/www/html/wp-includes/default-constants.php
+echo "define('WP_HOME', '$SITE_URL');" >> /var/www/html/wp-config.php
+echo "define('WP_SITEURL', '$SITE_URL');" >> /var/www/html/wp-config.php
 
 if [ -d "/var/www/jsarc" ] ; then
 rm -rf /var/www/html/wp-content/themes/jsarc
