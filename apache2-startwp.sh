af07d8b2ad8e8bd8c8c8729172760da306b64d42
Andrew Tarry <andrew.tarry@6point6.co.uk>
2019-01-29 10:19:18 +0000

    remove the plugin source code

--- apache2-startwp.sh
+++ apache2-startwp.sh
@@ -1,18 +1,33 @@
 #!/usr/bin/env bash
 
+function wp_plugin_install() {
+/bin/sh -c "wp plugin install $1 --activate"
+}
 
 cp -r /var/www/themes/jsarc /var/www/html/wp-content/themes/
 
-#echo "define('UPLOADS', '' . 'images');" >> /var/www/html/wp-config.php
-echo "define('WP_PLUGIN_DIR', '' . '../wp-content/themes/jsarc/plugins');" >> /var/www/html/wp-includes/default-constants.php
-echo "define('WP_PLUGIN_URL', '' . '/wp-content/themes/jsarc/plugins');" >> /var/www/html/wp-includes/default-constants.php
-echo "define('WP_HOME', 'https://web.notprod.jsarc.homeoffice.gov.uk');" >> /var/www/html/wp-includes/default-constants.php
-echo "define('WP_SITEURL', 'https://web.notprod.jsarc.homeoffice.gov.uk');" >> /var/www/html/wp-includes/default-constants.php
+echo "define('WP_HOME', '$SITE_URL');" >> /var/www/html/wp-includes/default-constants.php
+echo "define('WP_SITEURL', '$SITE_URL');" >> /var/www/html/wp-includes/default-constants.php
 
 if [ -d "/var/www/jsarc" ] ; then
 rm -rf /var/www/html/wp-content/themes/jsarc
 ln -s /var/www/jsarc /var/www/html/wp-content/themes/jsarc
 fi
 
+if [ "$DEV_DEPLOYMENT" = "yes" ]; then
+    wp core install \
+    --title="$SITE_TITLE" \
+    --url="$SITE_URL" \
+    --admin_user="$ADMIN_USER" \
+    --admin_email="$ADMIN_EMAIL" \
+    --admin_password="$ADMIN_PASSWORD"
+fi
+
+wp_plugin_install wordpress-importer
+wp_plugin_install /var/www/html/wp-content/themes/jsarc/plugins/acf-theme-code-pro.zip
+wp_plugin_install ilab-media-tools
+wp_plugin_install wp-export-menus
+wp_plugin_install wp-mail-smtp
+wp_plugin_install disable-xml-rpc
 
 $@
--- website-content/themes/jsarc/plugins/ilab-media-tools/vendor/trntv/probe/tests/bootstrap.php
+++
@@ -1,4 +0,0 @@
-<?php
-// ensure we get report on all possible php errors
-error_reporting(-1);
-$loader = require_once(__DIR__ . '/../vendor/autoload.php');
