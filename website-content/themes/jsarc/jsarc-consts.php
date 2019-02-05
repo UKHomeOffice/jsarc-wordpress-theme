81de47d195e2beb76c9d44854d819d2dc934e6fb
Andrew Tarry <andrew.tarry@6point6.co.uk>
2019-02-05 14:56:00 +0000

    add require

--- website-content/themes/jsarc/jsarc-consts.php
+++ website-content/themes/jsarc/jsarc-consts.php
@@ -1,19 +1,9 @@
 <?php
-function getSiteUrl() {
-    if(isset($_ENV['SITE_URL'])) {
-        return $_ENV['SITE_URL'];
-    }
-
-    return trim(file_get_contents('/var/www/html/wp-content/siteurl.txt'));
-}
-if (!defined('WP_DEBUG')) {
-    define('WP_DEBUG', true);
-}
 
 if (!defined('WP_HOME')) {
-    define('WP_HOME', getSiteUrl());
+    define('WP_HOME', $_ENV['SITE_URL']);
 }
 
 if (!defined('WP_SITEURL')) {
-    define('WP_SITEURL', getSiteUrl());
+    define('WP_SITEURL', $_ENV['SITE_URL']);
 }
