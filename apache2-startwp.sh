b390902688cffba52acd1c589bcaa896ff80d091
Andrew Tarry <andrew.tarry@6point6.co.uk>
2019-02-13 14:35:52 +0000

    add redirect

--- apache2-startwp.sh
+++ apache2-startwp.sh
@@ -34,4 +34,9 @@ wp_plugin_install disable-xml-rpc
 
 wp theme activate jsarc
 
+if [[ "$ADMIN_DEPLOYMENT" == "no" ]]; then
+echo "Redirect 301 /wp-login.php https://$SITE_URL/" >> /var/www/html/.htaccess
+echo "Redirect 301 /wp-admin/ https://$SITE_URL/" >> /var/www/html/.htaccess
+fi
+
 $@
